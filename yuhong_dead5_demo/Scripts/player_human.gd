7240dc8fa15d429209f8037806460659e47d3e15
joehywu <joehywu@tencent.com>
2025-07-21 20:56:00 +0800

    Create yuhong demo

---
+++ yuhong_dead5_demo/Scripts/player_human.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+const SPEED = 600
+#const JUMP_VELOCITY = -400.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _physics_process(delta: float) -> void:
+	
+	#上下左右移动
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.y = direction.y * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	move_and_slide()
