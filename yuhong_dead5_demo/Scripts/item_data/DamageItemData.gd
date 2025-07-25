e8e8fa9259c75dac484885714247bd96324ccf0c
FRANKYHYU-PC4\frankyhyu <903128002@qq.com>
2025-07-25 16:00:43 +0800

    【0725】实现了背包、道具随机生成、拾取和丢弃

---
+++ yuhong_dead5_demo/Scripts/item_data/DamageItemData.gd
@@ -0,0 +1,25 @@
+extends ConsumableData
+##伤害类道具类
+class_name DamageItem
+
+@export var drop_model: PackedScene
+
+## 物品被使用时调用
+func use() -> bool:
+	if current_amount > 0:
+		var consumed_amount = consume()
+		if consumed_amount > 0:
+			current_amount -= consumed_amount
+			if current_amount <= 0:
+				return destroy_if_empty
+	return false
+	print("used")
+
+## 消耗方法，需重写，返回消耗数量（>=0）
+func consume() -> int:
+	push_warning("[Override this function] consumable item [%s] has been consumed" % item_name)
+	return 1
+	
+func drop() -> void:
+	print("drop()called")
+	gamemanager.player.throw_item(self)
